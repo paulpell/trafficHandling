82042a1a5b837d7708866c98c957aace1566228a
gcmalloc <malik.bougacha@epfl.ch>
2011-06-10 13:38:22 +0200

    adding a way to check the md5 hash

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 if [ ! -d "build" ]; then 
 	echo "the binary directory didn't exist"
-	echo "I'm building it"
+	echo "I'm building it for you"
+	if [ $(whereis md5sum) ]; then 
+		echo "you have to verify each file in the directory"
+		echo "I'm doing it in case of something bad happens"
+		md5sum -c check.md5
+		if [ $? -ne 0 ]; then 
+			echo "the file are corrupted please download the project again" 1>&2
+			exit 2
+		fi
 	ant build
+	fi
 fi
 if [ $? -ne 0 ]; then 
 	echo "ant has issue : exiting " 1>&2 
