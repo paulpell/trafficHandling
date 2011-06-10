d86ff03923612c5a80a73a8a8eeabcb477adad7b
gcmalloc <malik.bougacha@epfl.ch>
2011-06-10 13:54:48 +0200

    last commit

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 if [ ! -d "build" ]; then 
 	echo "the binary directory didn't exist"
 	echo "I'm building it for you"
-	if [ $(whereis md5sum) ]; then 
+	if [ ! -z "$(type -P md5sum)" ]; then 
 		echo "you have to verify each file in the directory"
 		echo "I'm doing it in case of something bad happens"
 		md5sum -c check.md5
@@ -10,8 +10,8 @@ if [ ! -d "build" ]; then
 			echo "the file are corrupted please download the project again" 1>&2
 			exit 2
 		fi
-	ant build
 	fi
+	ant build
 fi
 if [ $? -ne 0 ]; then 
 	echo "ant has issue : exiting " 1>&2 
