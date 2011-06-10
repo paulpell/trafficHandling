3ef103f09c55d62b2878ceb88dd8eaaea73121f8
gcmalloc <malik.bougacha@epfl.ch>
2011-06-10 14:19:21 +0200

    adding something in md5 hash

--- md5gen.sh
+++ md5gen.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-find . -type f -print0 | xargs -0 md5sum >> check.md5
-sed check.md5 -e "s/.*.git.*//" | sed -e "s/.*check\.md5.*//" | sed -e "s/.*md5gen.*//" | sed '/^$/d' >> check2.md5
-mv check2.md5 check.md5
+find . -type f -print0 -name '*.java'| xargs -0 md5sum >> check.md5
 
